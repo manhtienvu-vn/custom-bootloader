7b8baf1463813d312d7d2c55d9999f89d4f21ba7
manhtienvu-vn <manhtienvu.vn@gmail.com>
2026-05-12 09:15:01 +0700

    set up simple custom-bootloader project and application project

---
+++ custom-bootloader/Core/Inc/flash_layout.h
@@ -0,0 +1,15 @@
+/*
+ * flash_layout.h
+ *
+ *  Created on: May 12, 2026
+ *      Author: ADMIN
+ */
+
+#ifndef INC_FLASH_LAYOUT_H_
+#define INC_FLASH_LAYOUT_H_
+
+#define BL_START_ADDR			0x08000000  //16KB
+#define APP_HEADER_ADDR			0x08004000  //1KB
+#define APP_START_ADDR  		0x08004400  //47B
+
+#endif /* INC_FLASH_LAYOUT_H_ */
